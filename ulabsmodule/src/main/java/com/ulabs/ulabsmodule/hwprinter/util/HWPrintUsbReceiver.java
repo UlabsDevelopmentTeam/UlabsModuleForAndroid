80f88836ac1cbd85383dc7e37ff53dad9288aed8
ljm2006 <ardentjm@gmail.com>
2018-02-13 09:26:32 +0900

    version 3.3.1 update  hwprinter  - USBBroadcastReceiver 로직 수정  - OnUsbPermissionGrantedListener 삭제

--- ulabsmodule/src/main/java/com/ulabs/ulabsmodule/hwprinter/util/HWPrintUsbReceiver.java
+++ ulabsmodule/src/main/java/com/ulabs/ulabsmodule/hwprinter/util/HWPrintUsbReceiver.java
@@ -1,29 +1,35 @@
 package com.ulabs.ulabsmodule.hwprinter.util;
 
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
+import android.util.Log;
 
 /**
  * Created by OH-Biz on 2018-02-12.
  */
 
 public class HWPrintUsbReceiver extends BroadcastReceiver {
-    public static final String ACTION_USB_ATTACHED = UsbManager.ACTION_USB_ACCESSORY_ATTACHED;
-    private OnUsbPermissionGrantedListener listener;
+    public static final String ACTION_USB_PERMISSION = "com.ulabsmodule.hwprinter.util.USB_PERMISSION";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        if(ACTION_USB_ATTACHED.equals(action)){
+        if(ACTION_USB_PERMISSION.equals(action)){
+            Log.d("ljm2006_UsbReceiver", "PrintReceiver onReceived");
             synchronized (this){
                 UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-                PendingIntent permissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_ATTACHED), 0);
-                HWPrintDriver driver = new HWPrintDriver(context);
-                driver.requestPermission(device, permissionIntent);
+
+                if(intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)){
+                    if(device != null){
+                        HWPrintDriver driver = new HWPrintDriver(context);
+                        driver.setDevice();
+                    }
+                }else{
+                    Log.d("ljm2006_UsbReceiver", "USB Permission denied...");
+                }
             }
         }
     }
--- ulabsmodule/src/main/java/com/ulabs/ulabsmodule/hwprinter/util/OnUsbPermissionGrantedListener.java
+++
@@ -1,9 +0,0 @@
-package com.ulabs.ulabsmodule.hwprinter.util;
-
-/**
- * Created by OH-Biz on 2018-02-12.
- */
-
-public interface OnUsbPermissionGrantedListener {
-    void permissionGranted();
-}
