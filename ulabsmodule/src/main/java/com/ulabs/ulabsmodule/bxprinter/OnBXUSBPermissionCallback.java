0777875af4259cd8fe235a4a20f21925eccf5f59
ljm2006 <ardentjm@gmail.com>
2019-02-18 10:50:30 +0900

    version 6.0.0 Updated! 프린터 관련 로직 전부 삭제 관련 로직은 다른 프로젝트로 이관되었음 -> ulabsPrinterModule 참조

--- ulabsmodule/src/main/java/com/ulabs/ulabsmodule/bxprinter/OnBXUSBPermissionCallback.java
+++
@@ -1,9 +0,0 @@
-package com.ulabs.ulabsmodule.bxprinter;
-
-/**
- * Created by OH-Biz on 2018-03-02.
- */
-
-public interface OnBXUSBPermissionCallback {
-    void onPermissionGranted();
-}
--- ulabsmodule/src/main/java/com/ulabs/ulabsmodule/hwprinter/util/OnUSBPermissionCallback.java
+++
@@ -1,9 +0,0 @@
-package com.ulabs.ulabsmodule.hwprinter.util;
-
-/**
- * Created by OH-Biz on 2018-03-02.
- */
-
-public interface OnUSBPermissionCallback {
-    void onPermissionGranted();
-}
